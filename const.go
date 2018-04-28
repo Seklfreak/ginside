4165f9cbdcd98b59a6980313b435004c70fdc234
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-29 00:53:37 +0200

    initial commit

---
+++ const.go
@@ -0,0 +1,37 @@
+package ginside
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var (
+	// the gall base url
+	baseURL = "https://gall.dcinside.com"
+	// shows recommended posts for a board
+	boardRecommendedPath = func(id string, page int) string {
+		return baseURL + "/board/lists/?id=" + id + "&page=" + strconv.Itoa(page) + "&exception_mode=recommend"
+	}
+	// the format used by dcinside
+	dateFormat = "2006.01.02 15:04:05"
+
+	headerReferer = "http://gall.dcinside.com/"
+	headerAccept  = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+
+	// list of random user agents
+	userAgents = []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	}
+
+	// returns a random user agent from the list of user agents
+	randomUserAgent = func() string {
+		rand.Seed(time.Now().Unix())
+		return userAgents[rand.Intn(len(userAgents))]
+	}
+)
---
+++ main.go
@@ -0,0 +1,107 @@
+package ginside
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
+)
+
+// Post contains information about a single dcinside post
+type Post struct {
+	ID     string
+	Title  string
+	Author string
+	Date   time.Time
+	Hits   int
+	Votes  int
+	Link   string
+}
+
+// BoardRecommendedPosts returns the posts from the first page of a dcgall board
+func BoardRecommendedPosts(id string) (posts []Post, err error) {
+	// setup http request
+	client := &http.Client{
+		Timeout: time.Second * 30,
+	}
+	req, err := http.NewRequest("GET", boardRecommendedPath(id, 1), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Referer", headerReferer)
+	req.Header.Set("User-Agent", randomUserAgent())
+	req.Header.Set("Accept", headerAccept)
+
+	// do http request
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close() // nolint: errcheck
+	if res.StatusCode != 200 {
+		return nil, errors.New("unexpected status code: " + strconv.Itoa(res.StatusCode))
+	}
+
+	// parse html
+	mainDoc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse posts
+	entries := mainDoc.Find(".list_tbody .tb")
+
+	// parse post content
+	for _, entry := range entries.Nodes {
+		entryNode := goquery.NewDocumentFromNode(entry)
+		noticeID := entryNode.Find("td.t_notice").Text()
+		title := entryNode.Find("td.t_subject a").Text()
+		link, ok := entryNode.Find("td.t_subject a").Attr("href")
+		if !ok {
+			return nil, errors.New("unable to find link")
+		}
+		author, ok := entryNode.Find("td.t_writer").Attr("user_name")
+		if !ok {
+			return nil, errors.New("unable to find author")
+		}
+		dateText, ok := entryNode.Find("td.t_date").Attr("title")
+		if !ok {
+			return nil, errors.New("unable to find date")
+		}
+		date, err := time.Parse(dateFormat, dateText)
+		if err != nil {
+			return nil, err
+		}
+		hitsText := entryNode.Find("td.t_hits").Eq(0).Text()
+		hits, err := strconv.Atoi(hitsText)
+		if err != nil {
+			return nil, err
+		}
+		votesText := entryNode.Find("td.t_hits").Eq(1).Text()
+		votes, err := strconv.Atoi(votesText)
+		if err != nil {
+			return nil, err
+		}
+
+		// skip announcements
+		if noticeID == "공지" {
+			continue
+		}
+
+		// add to list of posts
+		posts = append(posts, Post{
+			ID:     noticeID,
+			Title:  title,
+			Author: author,
+			Date:   date,
+			Hits:   hits,
+			Votes:  votes,
+			Link:   baseURL + link,
+		})
+	}
+
+	return posts, nil
+}
